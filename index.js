790d2398c2a17a2e3d6e98b6c4037714b54bc0b4
Emily Vu <evu725@gmail.com>
2025-10-01 11:04:58 -0400

    completed printSquareNumbers function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
-  //write code here
+  let total = 0;
 
+  for (let i = 1; i <= n; i++) {
+    console.log(i * i)
+    total += (i * i)
+  }
+  return total
 }
-printSquareNumbers(5);
+
+console.log(printSquareNumbers(5));
 //will print: 1, 4, 9, 16, 25
 // Final output is: 55
